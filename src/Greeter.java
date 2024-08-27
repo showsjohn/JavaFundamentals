da23a8f84ff8b8b4fca97858343d79e0e09153f6
John Shows <showsjohn@gmail.com>
2024-08-27 01:46:03 -0500

    renamed Main file and class to Greeter

--- src/Greeter.java
+++ src/Greeter.java
@@ -6,8 +6,8 @@
  *      Hello World example project
 ***************************************/
 
-public class Main {
+public class Greeter {
     public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    System.out.println("Hello World!");
     }
 }
