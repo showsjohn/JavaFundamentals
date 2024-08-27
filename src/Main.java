5ec6ada4a10b50107fbfd0976475659878fcfc97
John Shows <showsjohn@gmail.com>
2024-08-27 01:36:42 -0500

    Initial commit for first assignment

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**************************************
+ * Name: John Shows
+ * Date: 8/27/2024
+ * Java OOP 3381
+ * Description:
+ *      Hello World example project
+***************************************/
+
+public class Main {
+    public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    }
+}
